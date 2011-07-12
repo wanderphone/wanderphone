08db35494af00fcb584419ad88a024d7c9fc73e5
k7inas <k7inas@gmail.com>
2011-07-12 17:20:44 +0800

    douying 7-12

---
+++ douying7_12_pm/douying/src/com/wanderphone/douying/ViewCommentActivity.java
@@ -0,0 +1,180 @@
+package com.wanderphone.douying;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.widget.AbsListView;
+import android.widget.AbsListView.OnScrollListener;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.RelativeLayout.LayoutParams;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gdata.data.douban.ReviewFeed;
+import com.mobclick.android.MobclickAgent;
+import com.wanderphone.getxml.ConvertUtil;
+import com.wanderphone.getxml.MovieSubject;
+import com.wanderphone.getxml.ReviewSubject;
+import com.wanderphone.myAdapter.ReviewListAdapter;
+
+/**
+ * @ClassName: ViewCommentActivity
+ * @Description: 显示电影评论ListView
+ * @author：
+ * @version：v1.0
+ */
+public class ViewCommentActivity extends BaseListActivity {
+
+	private static final String ORDERBY = "score";// 按评分排序
+	private List<ReviewSubject> reviews = new ArrayList<ReviewSubject>();
+	private ReviewListAdapter listAdapter;
+	private int index = 1;
+	private int count = 10; // 每次获取数目
+	private int total; // 最大条目数
+	private boolean isFilling = false; // 判断是否正在获取数据
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		MobclickAgent.onError(this);
+
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.comment);
+
+		Bundle extras = getIntent().getExtras();
+		TextView movie_title_view = (TextView) findViewById(R.id.myTitle);
+		final MovieSubject subject = extras != null ? (MovieSubject) extras
+				.getSerializable("subject") : null;
+
+		Log.v("tests", subject.getId());
+		String title = subject.getTitle();
+		movie_title_view.setText(title);
+
+		fillDataBySubject(subject);
+
+		/**
+		 * 判断ListView滚动到底部
+		 */
+		getListView().setOnScrollListener(new OnScrollListener() {
+			public void onScroll(AbsListView view, int firstVisibleItem,
+					int visibleItemCount, int totalItemCount) {
+			}
+
+			public void onScrollStateChanged(AbsListView view, int scrollState) {
+				if (scrollState == OnScrollListener.SCROLL_STATE_IDLE) {
+					if (view.getLastVisiblePosition() == (view.getCount() - 1)) {
+						loadRemnantListItem(subject);
+					}
+				}
+			}
+
+		});
+	}
+
+	/**
+	 * @Title: loadRemnantListItem
+	 * @Description: 加载更多评论
+	 * @param：subject
+	 * @return:
+	 * @throws:
+	 */
+	private void loadRemnantListItem(MovieSubject subject) {
+		if (isFilling) {
+			return;
+		}
+		index = index + count;
+		if (index > total) {
+			return;
+		}
+		RelativeLayout loading = (RelativeLayout) findViewById(R.id.loading);
+		LayoutParams lp = (LayoutParams) loading.getLayoutParams();
+		lp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		loading.setLayoutParams(lp);
+		fillDataBySubject(subject);
+	}
+
+	/**
+	 * @Title: fillDataBySubject
+	 * @Description: 获取评论数据
+	 * @param：subject
+	 * @return:
+	 * @throws:
+	 */
+	private void fillDataBySubject(final MovieSubject subject) {
+		new AsyncTask<MovieSubject, Void, ReviewFeed>() {
+
+			@Override
+			protected ReviewFeed doInBackground(MovieSubject... args) {
+				ReviewFeed feed = null;
+				MovieSubject subject = args[0];
+				try {
+
+					// getMovieReviews 豆瓣java客户端
+					feed = getDoubanService().getMovieReviews(subject.getId(),
+							index, count, ORDERBY);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				return feed;
+			}
+
+			@Override
+			protected void onPostExecute(ReviewFeed feed) {
+				super.onPostExecute(feed);
+				closeProgressBar();
+				if (feed != null) {
+					total = feed.getTotalResults();
+					reviews.addAll(ConvertUtil.ConvertReviews(feed, subject));
+					if (listAdapter == null) {
+						listAdapter = new ReviewListAdapter(
+								ViewCommentActivity.this, getListView(),
+								reviews);
+						setListAdapter(listAdapter);
+					} else {
+						listAdapter.notifyDataSetChanged();
+					}
+
+					if (reviews.size() == 0) {
+						Toast.makeText(ViewCommentActivity.this, "没有找到相关评论！",
+								Toast.LENGTH_LONG).show();
+					}
+
+				} else {
+					Toast.makeText(ViewCommentActivity.this, "数据加载失败！",
+							Toast.LENGTH_LONG).show();
+				}
+				isFilling = false;
+			}
+
+			@Override
+			protected void onPreExecute() {
+				super.onPreExecute();
+				showProgressBar();
+				isFilling = true;
+			}
+
+		}.execute(subject);
+	}
+
+	/**
+	 * @Title: onListItemClick
+	 * @Description: 点击ListView事件
+	 * @param：l, v, position, id
+	 * @return:
+	 * @throws:
+	 */
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		Intent i = new Intent(this, CommentDetailActivity.class);
+		ReviewSubject review = reviews.get(position);
+		i.putExtra("review", review);
+		startActivity(i);
+	}
+}
---
+++ douying7_12_pm/douying/src/com/wanderphone/sqlite/DataBaseAdapter.java
@@ -0,0 +1,308 @@
+package com.wanderphone.sqlite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DatabaseUtils.InsertHelper;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.wanderphone.getxml.MovieSubject;
+
+public class DataBaseAdapter {
+
+	// 表中一条数据的名称
+	public static final String KEY_ID = "_id";
+	// 表中一条数据的内容
+	public static final String KEY_NUM = "num";
+	public static final String KEY_NAME = "name";
+	public static final String KEY_TYPE = "type";
+	public static final String KEY_DURATION = "duration";
+	public static final String KEY_PUB_AREA = "pub_area";
+	public static final String KEY_CINEMA = "cinema";
+	public static final String KEY_RATING = "rating";
+	public static final String KEY_IMG = "img";
+	public static final String KEY_MOVIEID = "movie_id";
+	public static final String KEY_CONTENT = "content";
+	// 表中一条数据的id
+	public static final String KEY_DATA = "data";
+	// 数据库名称为data
+	private static final String DB_NAME = "douying.db";
+	// 数据库表名
+	private static final String DB_TABLE = "nowplaying";
+
+	private static final String DB_TABLE_WEEK = "week";
+	private static final String DB_TABLE_BEST = "best";
+	// 数据库版本
+	private static final int DB_VERSION = 3;
+	// 本地Context对象
+	private Context mContext = null;
+	// 创建一个表
+	private static final String DB_CREATE_NP = "create table " + DB_TABLE
+			+ " (" + KEY_ID + " integer primary key autoincrement," + KEY_NAME
+			+ " text," + KEY_TYPE + " text," + KEY_DURATION + " text,"
+			+ KEY_PUB_AREA + " text," + KEY_RATING + " text," + KEY_CINEMA
+			+ " text," + KEY_IMG + " blbo)";
+	private static final String DB_CREATE_WEEK = "create table "
+			+ DB_TABLE_WEEK + "(" + KEY_ID
+			+ " integer primary key autoincrement," + KEY_NAME + " text,"
+			+ KEY_CONTENT + " text," + KEY_IMG + " blbo)";
+
+	private static final String DB_CREATE_BEST = "create table "
+			+ DB_TABLE_BEST + "(" + KEY_ID
+			+ " integer primary key autoincrement," + KEY_NAME + " text,"
+			+ KEY_IMG + " blbo)";
+
+	// 执行open()打开数据库时，保存返回的数据库对象
+	public SQLiteDatabase mSQLiteDatabase = null;
+	// 由SQLiteOpenHelper继承过来
+	public DatabaseHelper mDatabaseHelper = null;
+
+	// 使用SQLiteOpenHelper
+	public static class DatabaseHelper extends SQLiteOpenHelper {
+		// 构造函数-创建一个数据库
+		DatabaseHelper(Context context) {
+			// 当调用getWritableDatabase()或 getReadableDatabase()方法时，创建一个数据库
+			super(context, DB_NAME, null, DB_VERSION);
+		}
+
+		// 创建一个表
+		@Override
+		public void onCreate(SQLiteDatabase db) {
+			// 数据库没有表时创建一个表
+			db.execSQL(DB_CREATE_NP);
+			db.execSQL(DB_CREATE_WEEK);
+			db.execSQL(DB_CREATE_BEST);
+			Log.v("create", DB_CREATE_NP);
+			Log.v("create", DB_CREATE_WEEK);
+			Log.v("create", DB_CREATE_BEST);
+		}
+
+		// 升级数据库，实际项目中不使用此方法，会丢失原数据
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			db.execSQL("drop table if exists nowplaying");
+			db.execSQL("drop table if exists week");
+			db.execSQL("drop table if exists best");
+			// onCreate(db);
+			onCreate(db);
+		}
+	}
+
+	public boolean if_best_exists() {
+		Cursor cursor = mSQLiteDatabase.query(DB_TABLE_BEST, new String[] {
+				KEY_NAME}, null, null, null, null, null);
+		if (cursor.getCount() == 0) {
+			cursor.close();
+			return false;
+		} else {
+			cursor.close();
+			return true;
+		}
+
+	}
+	public boolean if_week_exists() {
+
+		Cursor cursor = mSQLiteDatabase.query(DB_TABLE_WEEK, new String[] {
+				KEY_NAME}, null, null, null, null, null);
+		if (cursor.getCount() == 0) {
+			cursor.close();
+			return false;
+		} else {
+			cursor.close();
+			return true;
+		}
+
+	}
+	public boolean if_np_exists() {
+
+		Cursor cursor = mSQLiteDatabase.query(DB_TABLE, new String[] {
+				KEY_NAME, KEY_TYPE, KEY_DURATION, KEY_PUB_AREA, KEY_CINEMA,
+				KEY_RATING }, null, null, null, null, null);
+		if (cursor.getCount() == 0) {
+			cursor.close();
+			return false;
+		} else {
+			cursor.close();
+			return true;
+		}
+
+	}
+
+	// 构造函数-取得Context
+	public DataBaseAdapter(Context context) {
+		mContext = context;
+	}
+
+	// 打开数据库，返回数据库对象
+	public void open() throws SQLException {
+		mDatabaseHelper = new DatabaseHelper(mContext);
+		mSQLiteDatabase = mDatabaseHelper.getWritableDatabase();
+	}
+
+	// 关闭数据库
+	public void close() {
+		mDatabaseHelper.close();
+	}
+	public void delete_all(){
+		String sql_delete = "delete from nowplaying";
+		String sql_delete1 = "delete from week";
+		String sql_delete2 = "delete from best";
+		mSQLiteDatabase.execSQL(sql_delete);
+		mSQLiteDatabase.execSQL(sql_delete1);
+		mSQLiteDatabase.execSQL(sql_delete2);
+
+	}
+	public void delete_all_np() {
+		String sql_delete = "delete from nowplaying";
+		mSQLiteDatabase.execSQL(sql_delete);
+	}
+
+	public void delete_all_week() {
+		String sql_delete = "delete from nowplaying";
+		mSQLiteDatabase.execSQL(sql_delete);
+	}
+
+	public void delete_all_best() {
+		String sql_delete = "delete from nowplaying";
+		mSQLiteDatabase.execSQL(sql_delete);
+	}
+
+	public long insertWeekData(String name, String content, byte[] img) {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_NAME, name);
+		initialValues.put(KEY_CONTENT, content);
+		initialValues.put(KEY_IMG, img);
+
+		return mSQLiteDatabase.insert(DB_TABLE_WEEK, KEY_ID, initialValues);
+
+	}
+
+	public long insertBestData(String name, byte[] img) {
+		ContentValues initialValues = new ContentValues();
+		initialValues.put(KEY_NAME, name);
+		initialValues.put(KEY_IMG, img);
+		return mSQLiteDatabase.insert(DB_TABLE_BEST, KEY_ID, initialValues);
+
+	}
+
+	public List<MovieSubject> loadWeekData() {
+		Cursor cursor = mSQLiteDatabase.query(DB_TABLE_WEEK, new String[] {
+				KEY_NAME, KEY_CONTENT, KEY_IMG }, null, null, null, null, null);
+		List<MovieSubject> movies = new ArrayList<MovieSubject>();
+		while (cursor.moveToNext()) {
+			MovieSubject movie = new MovieSubject();
+			movie.setTitle(cursor.getString(0));
+			movie.setDescription(cursor.getString(1));
+			movie.set_img_bytes(cursor.getBlob(2));
+			movies.add(movie);
+		}
+		cursor.close();
+		return movies;
+	}
+
+	public List<MovieSubject> loadBestData() {
+		Cursor cursor = mSQLiteDatabase.query(DB_TABLE, new String[] {
+				KEY_NAME, KEY_IMG }, null, null, null, null, null);
+		List<MovieSubject> movies = new ArrayList<MovieSubject>();
+		while (cursor.moveToNext()) {
+			MovieSubject movie = new MovieSubject();
+			movie.setTitle(cursor.getString(0));
+			movie.set_img_bytes(cursor.getBlob(1));
+			movies.add(movie);
+		}
+		cursor.close();
+		return movies;
+	}
+
+	// 插入一条数据
+	public long insertNpData(String name, String type, String duration,
+			String pub_area, String cinema, String rating, byte[] img) {
+		ContentValues initialValues = new ContentValues();
+
+		initialValues.put(KEY_NAME, name);
+		initialValues.put(KEY_TYPE, type);
+		initialValues.put(KEY_DURATION, duration);
+		initialValues.put(KEY_PUB_AREA, pub_area);
+		initialValues.put(KEY_CINEMA, cinema);
+		initialValues.put(KEY_RATING, rating);
+		initialValues.put(KEY_IMG, img);
+		return mSQLiteDatabase.insert(DB_TABLE, KEY_ID, initialValues);
+	}
+
+	public List<MovieSubject> loadNpData() {
+		Cursor cursor = mSQLiteDatabase.query(DB_TABLE, new String[] {
+				KEY_NAME, KEY_TYPE, KEY_DURATION, KEY_PUB_AREA, KEY_CINEMA,
+				KEY_RATING, KEY_IMG }, null, null, null, null, null);
+		List<MovieSubject> movies = new ArrayList<MovieSubject>();
+		while (cursor.moveToNext()) {
+			MovieSubject movie = new MovieSubject();
+			movie.setTitle(cursor.getString(0));
+			movie.setType(cursor.getString(1));
+			movie.set_movie_duration(cursor.getString(2));
+			movie.set_pub_area(cursor.getString(3));
+			movie.setSummary(cursor.getString(4));
+			movie.setRating(Float.valueOf(cursor.getString(5)));
+			movie.set_img_bytes(cursor.getBlob(6));
+			movies.add(movie);
+		}
+		cursor.close();
+		return movies;
+	}
+
+	// 删除一条数据
+	public boolean deleteData(long rowId) {
+		return mSQLiteDatabase.delete(DB_TABLE, KEY_ID + "=" + rowId, null) > 0;
+	}
+
+	// 删除一条数据
+	public void deleteAll() {
+		mSQLiteDatabase.delete(DB_TABLE, null, null);
+	}
+
+	// 通过Cursor查询所有数据
+	public Cursor fetchAllData() {
+		Cursor cursor = mSQLiteDatabase.query(DB_TABLE, new String[] {
+				KEY_NAME, KEY_TYPE, KEY_DURATION, KEY_PUB_AREA, KEY_CINEMA,
+				KEY_RATING }, null, null, null, null, null);
+		while (cursor.moveToNext()) {
+			Log.v("cursor", " name: " + cursor.getString(0) + " type: "
+					+ cursor.getString(1) + " duration: " + cursor.getString(2)
+					+ " pub_area: " + cursor.getString(3) + " cinema: "
+					+ cursor.getString(4) + " rating: " + cursor.getString(5));
+		}
+		Log.v("cursor", String.valueOf(cursor.getColumnCount()));
+		Log.v("cursor", String.valueOf(cursor.getCount()));
+
+		cursor.close();
+		return mSQLiteDatabase.query(DB_TABLE, new String[] { KEY_NAME,
+				KEY_TYPE, KEY_DURATION, KEY_PUB_AREA, KEY_CINEMA, KEY_RATING },
+				null, null, null, null, null);
+	}
+
+	// 查询指定数据
+	public Cursor fetchData(long rowId) throws SQLException {
+		Cursor mCursor = mSQLiteDatabase.query(true, DB_TABLE, new String[] {
+				KEY_ID, KEY_NUM, KEY_DATA }, KEY_ID + "=" + rowId, null, null,
+				null, null, null);
+		if (mCursor != null) {
+			mCursor.moveToFirst();
+		}
+		return mCursor;
+	}
+
+	// 更新一条数据
+	public boolean updateData(long rowId, int num, String data) {
+		ContentValues args = new ContentValues();
+		args.put(KEY_NUM, num);
+		args.put(KEY_DATA, data);
+		return mSQLiteDatabase.update(DB_TABLE, args, KEY_ID + "=" + rowId,
+				null) > 0;
+	}
+
+}
